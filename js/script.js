80cc5a24ec2d99ba3905a8870243cc07206d7e52
enterkvas <alexanderbugakov66@gmail.com>
2022-06-16 13:52:36 -0300

    apollo_grid

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+// Инициализация Swiper:
+const swiper = new Swiper('slider-swiper', {
+   navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	},
+   // Пагинация:
+   pagination: {el: '.swiper-pagination',
+      // Буллеты:
+      clickable: true,
+   },
+   slidesPerView: 1,
+});
