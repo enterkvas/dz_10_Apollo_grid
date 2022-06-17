ffde5fce16f0b82ad62f8ccde55d1650b2c28fb2
enterkvas <alexanderbugakov66@gmail.com>
2022-06-17 18:54:52 -0300

    the next benefits block

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
+// "use strict";
+// // Инициализация Swiper:
+// const swiper = new Swiper('slider-swiper', {
+//    navigation: {
+// 		nextEl: '.swiper-button-next',
+// 		prevEl: '.swiper-button-prev'
+// 	},
+//    // Пагинация:
+//    pagination: {el: '.swiper-pagination',
+//       // Буллеты:
+//       clickable: true,
+//    },
+//    slidesPerView: 1,
+// });
+
 "use strict";
 // Инициализация Swiper:
-const swiper = new Swiper('slider-swiper', {
-   navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev'
+const swiper = new Swiper('.slider-swiper', {
+	// navigation: {
+	// 	nextEl: '.swiper-button-next',
+	// 	prevEl: '.swiper-button-prev'
+	// },
+	// Пагинация:
+	pagination: {el: '.swiper-pagination',
+	//Буллеты:	
+	clickable: true,
 	},
-   // Пагинация:
-   pagination: {el: '.swiper-pagination',
-      // Буллеты:
-      clickable: true,
-   },
-   slidesPerView: 1,
+	slidesPerView: 1,
 });
